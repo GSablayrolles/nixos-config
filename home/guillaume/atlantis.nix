ff26a598a5dd1b7ec18c7c506f4759030f66b8bb
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2024-06-19 15:01:08 +0200

    Fix: Add monitors for Atlantis

--- home/guillaume/atlantis.nix
+++ home/guillaume/atlantis.nix
@@ -36,6 +36,23 @@
     enable = true;
   };
 
+    monitors = [
+    {
+      name = "HDMI-A-1";
+      width = 1920;
+      height = 1080;
+      x = 0;
+      workspace = "1";
+    }
+    {
+      name = "DP-1";
+      width = 1920;
+      height = 1080;
+      x = 1920;
+      workspace = "2";
+      primary = true;
+    }
+  ];
 
     fontProfiles = {
     enable = true;
