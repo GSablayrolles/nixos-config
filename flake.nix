dfe52e3e8aa401b7aaa9342cd6ed59d12e6b2dac
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2024-12-22 22:34:59 +0100

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    hyprland-qtutils = {
+      url = "github:hyprwm/hyprland-qtutils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     stylix = {
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
