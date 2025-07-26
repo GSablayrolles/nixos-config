1d23812a6e5d93c89f38ef489545a16a55a2f178
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-07-26 12:49:17 +0200

    fix(swaync): little upgrade

--- home/guillaume/features/desktop/wayland-wm/swaync.nix
+++ home/guillaume/features/desktop/wayland-wm/swaync.nix
@@ -50,26 +50,26 @@ in
             "control-center-layer": "top",
             "layer-shell": true,
             "cssPriority": "application",
-            "control-center-margin-top": 0,
-            "control-center-margin-bottom": 0,
+            "control-center-width": 350,
+            "control-center-margin-top": 8,
+            "control-center-margin-bottom": 8,
             "control-center-margin-right": 0,
-            "control-center-margin-left": 0,
+            "control-center-margin-left": 8,
             "notification-2fa-action": true,
             "notification-inline-replies": true,
-            "notification-icon-size": 64,
-            "notification-body-image-height": 100,
-            "notification-body-image-width": 200,
-            "timeout": 10,
-            "timeout-low": 5,
-            "timeout-critical": 0,
+            "notification-window-width": 350,
+            "notification-icon-size": 60,
+            "notification-body-image-height": 180,
+            "notification-body-image-width": 180,
+            "timeout": 12,
+            "timeout-low": 6,
+            "timeout-critical": 1,
             "fit-to-screen": true,
             "relative-timestamps": true,
-            "control-center-width": 500,
-            "notification-window-width": 500,
             "keyboard-shortcuts": true,
             "image-visibility": "when-available",
             "transition-time": 200,
-            "hide-on-clear": true,
+            "hide-on-clear": false,
             "hide-on-action": true,
             "script-fail-notify": true,
             "notification-visibility": {
@@ -112,11 +112,11 @@ in
                         },
                         {
                             "label": "",
-                            "command": "neofetch"
+                            "command": "${pkgs.kitty}/bin/kitty "
                         },
                         {
                             "label": "",
-                            "command": "pfetch"
+                            "command": "${pkgs.kitty}/bin/kitty "
                         },
                         {
                             "label": "󰖩",
