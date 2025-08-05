6594fa4b0d138113c43ffa2efd57bb72c24bc967
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-08-05 17:38:06 +0200

    feat: add openssh

--- hosts/shared/global/default.nix
+++ hosts/shared/global/default.nix
@@ -6,6 +6,7 @@
   imports = [
     ./locale.nix
     ./nix.nix
+    ./openssh.nix
   ];
 
   nixpkgs = {
---
+++ hosts/shared/global/openssh.nix
@@ -0,0 +1,21 @@
+{ config, ... }:
+{
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    allowSFTP = true;
+
+    settings = {
+      PasswordAuthentication = false;
+      X11Forwarding = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
+
+  services.fail2ban = {
+    enable = true;
+    maxretry = 10;
+    bantime = "24h";
+    bantime-increment.enable = true;
+  };
+}
