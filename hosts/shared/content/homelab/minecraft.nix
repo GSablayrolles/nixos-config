f69bdd5f68b39d51bab025c68d8ab7db75141431
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-08-26 12:52:51 +0200

    fix(iss): add ops to  minecraft server

--- hosts/shared/content/homelab/minecraft.nix
+++ hosts/shared/content/homelab/minecraft.nix
@@ -1,9 +1,26 @@
 {
   pkgs,
-  lib,
   inputs,
   ...
 }:
+let
+  myOpsJson = pkgs.writeText "ops.json" (
+    builtins.toJSON [
+      {
+        uuid = "06c132fc-5ea6-49f0-a7a0-4712d2d8159c";
+        name = "ShardZ_";
+        level = 2;
+        bypassesPlayerLimit = true;
+      }
+      {
+        uuid = "40109128-7c9f-4c9a-b087-0b993f0dea47";
+        name = "Moooreo";
+        level = 2;
+        bypassesPlayerLimit = true;
+      }
+    ]
+  );
+in
 {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
@@ -23,9 +40,17 @@
           gameMode = "survival";
           difficulty = "normal";
           online-mode = true;
+          op-permission-level = 2;
 
           #server-port = 25565;
         };
+
+        jvmOpts = "-Xms2G -Xmx4G";
+
+        files = {
+          "ops.json" = myOpsJson;
+        };
+
       };
     };
   };
